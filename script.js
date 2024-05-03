c122f48a654e896e11f94334a110af17af271880
Mohammed <156168748+MohammedAlrasheidi@users.noreply.github.com>
2024-05-03 00:37:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -48,7 +48,7 @@ document.addEventListener('DOMContentLoaded', function() {
         const natoAlphabet = {
             'A': 'Alpha', 'B': 'Bravo', 'C': 'Charlie', 'D': 'Delta', 'E': 'Echo',
             'F': 'Foxtrot', 'G': 'Golf', 'H': 'Hotel', 'I': 'India', 'J': 'Juliett',
-            'K': 'Kilo', 'L': 'Lima', 'M': 'Mike', 'N': 'November', 'O': 'Oscar',
+            'K': 'Kilo', 'L': 'Lima', 'M': 'Mike', 'N': 'November', 'O': 'Ohh Wow',
             'P': 'Papa', 'Q': 'Quebec', 'R': 'Romeo', 'S': 'Sierra', 'T': 'Tango',
             'U': 'Uniform', 'V': 'Victor', 'W': 'Whiskey', 'X': 'X-ray', 'Y': 'Yankee',
             'Z': 'Zulu'
